724a10eb512b2dd1dbb56c2aaf01662ba9345460
Franz Wollang <franzwollang@gmail.com>
2022-12-06 11:51:56 +0100

    Create grid

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,42 @@
+import { coordToId } from "./utils";
+
 // grab DOM elements
-const currentPlayerDisplay = document.querySelector("#current-player");
-const currentScoreDisplay = document.querySelector("#current-score");
-const startButton = document.querySelector("#start-button");
-const resetButton = document.querySelector("#reset-button");
-const gameGrid = document.querySelector("#game-grid");
+const currentPlayerDisplay = document.querySelector(
+	"#current-player"
+) as HTMLElement;
+const currentScoreDisplay = document.querySelector(
+	"#current-score"
+) as HTMLElement;
+const startButton = document.querySelector("#start-button") as HTMLElement;
+const resetButton = document.querySelector("#reset-button") as HTMLElement;
+const gameGrid = document.querySelector("#game-grid") as HTMLElement;
 
 // constants
-const gridSize = 19;
+const gridSize = 21;
 
 // initialize state
 
 // setup grid
+gameGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+gameGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
 for (let row = 0; row < gridSize; row++) {
 	for (let col = 0; col < gridSize; col++) {
+		// create grid element
 		const gridElement = document.createElement("div");
+
+		// generate id
+		const id = coordToId([row, col]);
+
+		// set element attributes
+		gridElement.id = id;
+		gridElement.innerText = id;
+		gridElement.style.height = `${600 / gridSize - 2}px`;
+		gridElement.style.width = `${600 / gridSize - 2}px`;
+		gridElement.style.backgroundColor = "grey";
+
+		// add grid element to gameGrid
+		gameGrid?.appendChild(gridElement);
 	}
 }
 
