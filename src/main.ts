3d6ed1a4661b64f3db67b3434f5b915f31ff113b
Franz Wollang <franzwollang@gmail.com>
2022-12-06 11:12:25 +0100

    Add a types and utils file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
+// grab DOM elements
+const currentPlayerDisplay = document.querySelector("#current-player");
+const currentScoreDisplay = document.querySelector("#current-score");
+const startButton = document.querySelector("#start-button");
+const resetButton = document.querySelector("#reset-button");
+const gameGrid = document.querySelector("#game-grid");
+
+// constants
+const gridSize = 19;
+
+// initialize state
+
+// setup grid
+for (let row = 0; row < gridSize; row++) {
+	for (let col = 0; col < gridSize; col++) {
+		const gridElement = document.createElement("div");
+	}
+}
+
 export {};
